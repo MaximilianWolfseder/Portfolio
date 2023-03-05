d5a60c0f6297a2d8d9f70ee66bb16dbfd0264d5d
HerrGerbrandt1 <87241805+HerrGerbrandt1@users.noreply.github.com>
2023-03-05 15:04:37 +0100

    Add files via upload

---
+++ Projects/login/scriptIndex.js
@@ -0,0 +1,26 @@
+const database = [{username: "admin", password: "1234"}]
+
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    for (item of database) {
+        if (item.username === username && item.password === password) {
+            alert("logged in")
+            return;
+        }
+    }
+}
+
+function signUp() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    
+    database.push({username: username, password: password})
+    alert("Signed Up")
+}
+
+
+document.querySelector('.exit')?.addEventListener('click', (event) =>{
+    window.location.href = '/Portfolio/home.html#Projects';
+})
