8cb6e076874ca404226289ad677784715a903268
Maximilian Wolfseder <87241805+MaximilianWolfseder@users.noreply.github.com>
2023-04-01 19:50:33 +0200

    Add files via upload

---
+++ Projects/nodejs-backend/public/script.js
@@ -0,0 +1,32 @@
+
+//get Data from database
+/*
+getData()
+
+async function getData() {
+    const response = await fetch('/api');
+    const data = await response.json();
+
+    for (item of data) {
+
+        console.log(item)
+        
+    }
+}
+*/
+
+//Add Data to database
+/*
+    fetch('/api', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+        username: 'Junge',
+        password: 'Junge1234'
+        })
+    });
+*/
+
+
---
+++ Projects/nodejs-backend/server.js
@@ -0,0 +1,52 @@
+const { request } = require('express');
+const express = require('express')
+const app = express()
+
+
+//npm install nedb
+const Datastore = require("nedb")
+const database = new Datastore("database.db")
+database.loadDatabase()
+
+//Date Handlers
+const fullDate = Date()
+
+
+
+
+
+//find
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) =>{
+        if (err) {
+            response.end();
+            return;
+        }
+        response.json(data);
+    })
+});
+
+
+
+
+app.use(express.static("public"))
+
+app.set('view engine', 'ejs')
+
+
+app.get('/api', (req, res) => {
+    console.log("here")
+})
+
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)
+app.use(express.json({limit: '1mb'}));
+
+app.listen(3000)
+//insert
+app.post('/api', (request, response) => {
+    const data = request.body;
+    database.insert(data); 
+    console.log(request.body);
+});
