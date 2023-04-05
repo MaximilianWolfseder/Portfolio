254e9dffc8aeeaf8e0e6a2e255d3b06a212c1e04
Maximilian Wolfseder <87241805+MaximilianWolfseder@users.noreply.github.com>
2023-04-05 14:15:51 +0200

    Add files via upload

---
+++ Projects/carousel/script.js
@@ -0,0 +1,50 @@
+const arrowbuttons = document.querySelectorAll(".arrowbutton");
+const buttons = document.querySelectorAll(".heroButton");
+
+
+arrowbuttons.forEach((button =>{
+    button.addEventListener("click", (event) => {
+        const state = button.getAttribute('data-state')
+        if (state === 'previous') {
+            buttons.forEach((insideButton) => {
+                console.log("hello")
+                const index = insideButton.getAttribute("data-index")
+                let i = parseInt(index)
+                if (index === '3') {
+                    insideButton.setAttribute('data-index', "1")
+                } else {
+                    i++;
+                    insideButton.setAttribute('data-index', i.toString())
+                }
+            })
+        } else {
+            buttons.forEach((insideButton) => {
+                const index = insideButton.getAttribute("data-index")
+                let i = parseInt(index)
+                if (index === '1') {
+                    insideButton.setAttribute('data-index', "3")
+            } else {
+                i--;
+                insideButton.setAttribute('data-index', i.toString())
+            }
+            })
+            
+        }
+    })
+}))
+
+/*
+console.log(buttons)
+buttons.forEach(function (button) {
+    
+    button.addEventListener("click", function () {
+        
+        var currentState = button.getAttribute("data-state");
+        if (currentState === "false") {
+            button.setAttribute("data-state", "true");
+        }
+        else {
+            button.setAttribute("data-state", "false");
+        }
+    });
+});*/
